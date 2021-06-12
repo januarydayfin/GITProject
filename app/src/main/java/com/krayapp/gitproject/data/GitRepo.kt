de2aa0059a63494571cbc728e020a59de4e2edcb
krayman <50882897+krayman@users.noreply.github.com>
2021-06-12 21:13:27 +0700

    Реактивные данные

--- app/src/main/java/com/krayapp/gitproject/data/GitRepo.kt
+++ app/src/main/java/com/krayapp/gitproject/data/GitRepo.kt
@@ -1,19 +1,16 @@
 package com.krayapp.gitproject.data
 
+import io.reactivex.rxjava3.core.Observable
+
+
 class GitRepo {
-    private val repositories = mutableListOf(
-        GitUser("login1"),
-        GitUser("login2"),
-        GitUser("login3"),
-        GitUser("login4"),
-        GitUser("login5")
-    )
-
-    fun getUsers(): List<GitUser>{
-        return repositories
-    }
-
-    fun createNewUser(){
-        repositories.add(GitUser("loginNew"))
+    fun repositories():Observable<GitUser> {
+        return Observable.fromIterable(mutableListOf(
+            GitUser("login1"),
+            GitUser("login2"),
+            GitUser("login3"),
+            GitUser("login4"),
+            GitUser("login5"))
+        )
     }
 }
